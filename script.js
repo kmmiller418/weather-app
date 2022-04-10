f2269114e1bc579f47631eb555f3c7440679da40
kmiller <kmmiller418@gmail.com>
2022-04-10 15:04:55 -0400

    Add weather alert, add stylings

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-const submit = document.querySelector(".getForecast");
+const submitF = document.querySelector(".getForecastF");
+const submitC = document.querySelector(".getForecastC");
 
-submit.addEventListener("click", () => {
+submitF.addEventListener("click", (e) => {
+  const system = e.target.value;
   const zipcode = document.querySelector("#zipcode").value;
   const countryCode = document.querySelector("#countryCode").value;
 
   if (zipcode == "" || countryCode == "") {
-      alert("please enter a location")
-      return null;
+    alert("please enter a location");
+    return null;
   }
 
-  getWeather(zipcode, countryCode);
+  getWeather(zipcode, countryCode, system);
+});
+
+submitC.addEventListener("click", (e) => {
+  const system = e.target.value;
+  const zipcode = document.querySelector("#zipcode").value;
+  const countryCode = document.querySelector("#countryCode").value;
+
+  if (zipcode == "" || countryCode == "") {
+    alert("please enter a location");
+    return null;
+  }
+
+  getWeather(zipcode, countryCode, system);
 });
 
 //functions
-const getWeather = (zipcode, countryCode) => {
+const getWeather = (zipcode, countryCode, system) => {
   fetch(
-    `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${countryCode}&appid=1eba33578583cc2cb757872032783084`
+    `https://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${countryCode}&appid=1eba33578583cc2cb757872032783084`
   )
     .then((response) => response.json())
     .then((json) => {
@@ -24,26 +39,35 @@ const getWeather = (zipcode, countryCode) => {
       return { lat, lon, name };
     })
     .then((coordinates) => {
+      const unit = (system === 'f') ? "imperial" : "metric";
       fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=hourly&appid=1eba33578583cc2cb757872032783084&units=imperial`
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=hourly&appid=1eba33578583cc2cb757872032783084&units=${unit}`
       )
         .then((response) => response.json())
         .then((json) => {
-            console.log(json);
-            postForecast(json);
-            post3DayForecast(json);
+          console.log(json);
+          postForecast(json);
+          post3DayForecast(json);
         });
     });
 };
 
-postLocation = (city) => {
+const postLocation = (city) => {
   const location = document.querySelector(".location");
-  location.innerHTML = `Thanks for checking in from ${city}.`;
+  location.innerHTML = `Thanks for checking in from ${city}!`;
 };
 
-postForecast = (json) => {
+const postForecast = (json) => {
   const weatherBox = document.querySelector(".weather-box");
-  weatherBox.innerHTML = `<h3>Your forecast for today is:</h3>`;
+  weatherBox.innerHTML = `<h3>Your forecast for today</h3>`;
+
+  const { current, daily, alerts } = json;
+  const temps = [
+    current.temp,
+    daily[0].temp.max,
+    daily[0].temp.min,
+    daily[0].feels_like.day,
+  ];
 
   let currentDate = document.createElement("p");
   let city = document.createElement("p");
@@ -52,42 +76,77 @@ postForecast = (json) => {
   let tempHi = document.createElement("p");
   let tempLo = document.createElement("p");
   let feelsLike = document.createElement("p");
+  let humidity = document.createElement("p");
+  let windSpeed = document.createElement("p");
+  
+  let weatherAlerts = document.createElement("div");
+  weatherAlerts.classList.add('weather-alerts');
 
-  currentDate.innerHTML = `It is ${new Date()}`;
-  city.innerHTML = `Timezone: ${json.timezone}`;
-  temp.innerHTML = `Current temperature: ${json.current.temp} &#176F`;
-  currentConditions.innerHTML = `Currently the forecast is: ${json.current.weather[0].main}, with ${json.current.weather[0].description}`;
-  tempHi.innerHTML = `The high today is ${json.daily[0].temp.max} &#176F`;
-  tempLo.innerHTML = `The low today is ${json.daily[0].temp.min} &#176F`;
-  feelsLike.innerHTML = `During the day, it will feel around ${json.daily[0].feels_like.day} &#176F`;
+  currentDate.innerHTML = `Your weather report was generated on ${new Date()}`;
+  temp.innerHTML = `Current temperature: <span>${Math.round(
+    temps[0]
+  )} &#176F</span>`;
+  currentConditions.innerHTML = `Currently the forecast is: <span>${current.weather[0].main}, with ${current.weather[0].description}</span>`;
+  tempHi.innerHTML = `The high today is <span>${Math.round(
+    temps[1]
+  )} &#176F</span>`;
+  tempLo.innerHTML = `The low today is <span>${Math.round(
+    temps[2]
+  )} &#176F</span>`;
+  feelsLike.innerHTML = `During the day, it will feel around <span>${Math.round(
+    temps[3]
+  )} &#176F</span>`;
+  humidity.innerHTML = `The humidity today is <span>${current.humidity}%</span>`;
+  windSpeed.innerHTML = `The wind speed today is <span>${current.wind_speed} mph</span>`
+
+
+  if (!alerts) {
+    weatherAlerts.innerHTML = "<h2>No Weather Alerts Today! Have a good day!</h2>"
+  } else {
+    let senderName = document.createElement("h4");
+    let event = document.createElement("h4");
+    let description = document.createElement("p");
+
+    senderName.innerHTML = alerts.sender_name;
+    event.innerHTML = alerts.event;
+    description.innerHTML = alerts.description;
+
+    weatherAlerts.append(senderName, event, description)
+  }
 
   weatherBox.append(
     currentDate,
     city,
     temp,
-    currentConditions,
     tempHi,
     tempLo,
-    feelsLike
+    feelsLike,
+    currentConditions,
+    humidity,
+    windSpeed,
+    weatherAlerts
   );
 };
 
-post3DayForecast = (json) => {
-    const futureForecast = document.querySelector(".future-forecast");
-    futureForecast.innerHTML = `<h3>Your three day forecast is:</h3>`;
+const post3DayForecast = (json) => {
+  const futureForecast = document.querySelector(".future-forecast");
+  futureForecast.innerHTML = `<h3>Your three day forecast</h3>`;
 
-    for (i = 1; i < 4; i++) {
-        let hi = document.createElement("p");
-        let lo = document.createElement("p");
-        let condition = document.createElement("p");
-        let img = document.createElement("img");
-      
-        hi.innerHTML = `Day ${i} High: ${json.daily[i].temp.max} &#176F`;
-        lo.innerHTML = `Day ${i} Low: ${json.daily[i].temp.min} &#176F`;
-        condition.innerHTML = `The forecast calls for ${json.daily[i].weather[0].description}`;
-        img.src = `http://openweathermap.org/img/wn/${json.daily[i].weather[0].icon}@2x.png`
-        futureForecast.append(img, hi, lo, condition);
-    }
-  
-    futureForecast.style.border = "2px solid black";
-}
+  for (i = 1; i < 4; i++) {
+    let daily = json.daily[i];
+    let hiLo = document.createElement("p");
+    let condition = document.createElement("p");
+    let img = document.createElement("img");
+
+    hiLo.innerHTML = `<span>${Math.round(
+      daily.temp.max
+    )} &#176F / ${Math.round(
+      daily.temp.min
+    )} &#176F</span>`;
+    condition.innerHTML = `The forecast calls for ${daily.weather[0].description}`;
+    img.src = `https://openweathermap.org/img/wn/${daily.weather[0].icon}@2x.png`;
+    futureForecast.append(img, hiLo, condition);
+  }
+
+  futureForecast.style.border = "2px solid black";
+};
