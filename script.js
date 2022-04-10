2de6daf3c4ca54855fa2fe7d2550a9a6848cd152
kmiller <kmmiller418@gmail.com>
2022-04-10 15:10:46 -0400

    Fix output to correctly show C or F depending on selected unit

--- script.js
+++ script.js
@@ -39,15 +39,15 @@ const getWeather = (zipcode, countryCode, system) => {
       return { lat, lon, name };
     })
     .then((coordinates) => {
-      const unit = (system === 'f') ? "imperial" : "metric";
+      const unit = system === "F" ? "imperial" : "metric";
       fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=hourly&appid=1eba33578583cc2cb757872032783084&units=${unit}`
       )
         .then((response) => response.json())
         .then((json) => {
           console.log(json);
-          postForecast(json);
-          post3DayForecast(json);
+          postForecast(json, system);
+          post3DayForecast(json, system);
         });
     });
 };
@@ -57,7 +57,7 @@ const postLocation = (city) => {
   location.innerHTML = `Thanks for checking in from ${city}!`;
 };
 
-const postForecast = (json) => {
+const postForecast = (json, system) => {
   const weatherBox = document.querySelector(".weather-box");
   weatherBox.innerHTML = `<h3>Your forecast for today</h3>`;
 
@@ -78,30 +78,30 @@ const postForecast = (json) => {
   let feelsLike = document.createElement("p");
   let humidity = document.createElement("p");
   let windSpeed = document.createElement("p");
-  
+
   let weatherAlerts = document.createElement("div");
-  weatherAlerts.classList.add('weather-alerts');
+  weatherAlerts.classList.add("weather-alerts");
 
   currentDate.innerHTML = `Your weather report was generated on ${new Date()}`;
   temp.innerHTML = `Current temperature: <span>${Math.round(
     temps[0]
-  )} &#176F</span>`;
+  )} &#176${system}</span>`;
   currentConditions.innerHTML = `Currently the forecast is: <span>${current.weather[0].main}, with ${current.weather[0].description}</span>`;
   tempHi.innerHTML = `The high today is <span>${Math.round(
     temps[1]
-  )} &#176F</span>`;
+  )} &#176${system}</span>`;
   tempLo.innerHTML = `The low today is <span>${Math.round(
     temps[2]
-  )} &#176F</span>`;
+  )} &#176${system}</span>`;
   feelsLike.innerHTML = `During the day, it will feel around <span>${Math.round(
     temps[3]
-  )} &#176F</span>`;
+  )} &#176${system}</span>`;
   humidity.innerHTML = `The humidity today is <span>${current.humidity}%</span>`;
-  windSpeed.innerHTML = `The wind speed today is <span>${current.wind_speed} mph</span>`
-
+  windSpeed.innerHTML = `The wind speed today is <span>${current.wind_speed} mph</span>`;
 
   if (!alerts) {
-    weatherAlerts.innerHTML = "<h2>No Weather Alerts Today! Have a good day!</h2>"
+    weatherAlerts.innerHTML =
+      "<h2>No Weather Alerts Today! Have a good day!</h2>";
   } else {
     let senderName = document.createElement("h4");
     let event = document.createElement("h4");
@@ -111,7 +111,7 @@ const postForecast = (json) => {
     event.innerHTML = alerts.event;
     description.innerHTML = alerts.description;
 
-    weatherAlerts.append(senderName, event, description)
+    weatherAlerts.append(senderName, event, description);
   }
 
   weatherBox.append(
@@ -128,7 +128,7 @@ const postForecast = (json) => {
   );
 };
 
-const post3DayForecast = (json) => {
+const post3DayForecast = (json, system) => {
   const futureForecast = document.querySelector(".future-forecast");
   futureForecast.innerHTML = `<h3>Your three day forecast</h3>`;
 
@@ -140,9 +140,7 @@ const post3DayForecast = (json) => {
 
     hiLo.innerHTML = `<span>${Math.round(
       daily.temp.max
-    )} &#176F / ${Math.round(
-      daily.temp.min
-    )} &#176F</span>`;
+    )} &#176${system} / ${Math.round(daily.temp.min)} &#176${system}</span>`;
     condition.innerHTML = `The forecast calls for ${daily.weather[0].description}`;
     img.src = `https://openweathermap.org/img/wn/${daily.weather[0].icon}@2x.png`;
     futureForecast.append(img, hiLo, condition);
